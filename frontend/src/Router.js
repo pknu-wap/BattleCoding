44d6be22a2a6ebfae0120a12c0d8186c61086b57
Seol-Munhyeok <abc1234568@naver.com>
2025-03-31 21:06:09 +0900

    fix: 프론트엔드 페이지 Router 설정 수정
    모든 페이지가 한 페이지에 나오는 문제 해결

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,17 +1,18 @@
-import React, { useContext } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import App from './App';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
-
+import Game from './pages/Game/Game';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Main />} />
+                <Route path="/auth/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/game" element={<Game />} />
             </Routes>
         </BrowserRouter>
     );
