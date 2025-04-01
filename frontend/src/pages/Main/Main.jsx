b0bfe0cedbd4d8ee46e683982aa74a951715d470
PKJYJ <p134812024@gmail.com>
2025-04-01 23:23:10 +0900

    Mainpage 수정

--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import './Main.css';
 import logo2 from './logo/logo2.png';
 
-function Main() {
+const MainPage = () => {
   return (
-    <div id="logo-container">
-      <img id="logo-image" src={logo2} alt="logo2" />
-      <div id="button-container">
-        <button>게스트로 참여하기</button>
-        <button>로그인 하러가기</button>
+    <div className="container">
+      <div className="logo-container">
+        <img src={logo2} className="logo-image" />
+      </div>
+      <div className="button-container">
+        <button className="button">게스트로 참여하기</button>
+        <button className="button">로그인 하러가기</button>
       </div>
     </div>
   );
-}
+};
 
 export default Main;
