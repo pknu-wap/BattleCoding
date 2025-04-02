b93b68380b7b4228ce4afa71739bb00ac2a34143
PKJYJ <p134812024@gmail.com>
2025-04-02 19:03:13 +0900

    mainpage ui 설계 수정

--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -7,10 +7,10 @@ const Main = () => {
     <div className="container">
       <div className="logo-container">
         <img src={logo2} className="logo-image" />
-      </div>
-      <div className="button-container">
+        <div className="button-container">
         <button className="button">게스트로 참여하기</button>
         <button className="button">로그인 하러가기</button>
+      </div>
       </div>
     </div>
   );
