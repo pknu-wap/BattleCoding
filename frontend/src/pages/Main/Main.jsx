6db56348a36c952818480ffeb36c32d63dab2601
Jeong Yurim <yr61oo@naver.com>
2025-04-07 20:32:07 +0900

    feat: 메인 페이지 틀 수정

--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import './Main.css';
-import logo2 from './logo/logo2.png';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import QuizSection from './components/QuizSection';
+import './Main.scss';
 
 function Main() {
   return (
-    <div id="logo-container">
-      <img id="logo-image" src={logo2} alt="logo2" />
-      <div id="button-container">
-        <button>게스트로 참여하기</button>
-        <button>로그인 하러가기</button>
-      </div>
+    <div className='main'>
+      <Navbar />
+      
     </div>
   );
 }
