e1ccf8b38471e38526fde3af4abb709a5f36c39a
Jeon Juhyun <juh060415@gmail.com>
2025-04-10 20:43:04 +0900

    feat. 마이페이지 수정

--- frontend/src/pages/Game/MyPage/MyQuiz/MyQuiz.js
+++ frontend/src/pages/Game/MyPage/MyQuiz/MyQuiz.js
@@ -6,19 +6,19 @@ function MyQuiz() {
     <div className="MyQuiz">
       <div className="MyQuestion">
         <span className="MyAll">
-          푼 문제 수 <b>??</b> 개
+          푼 문제 수 <b className="aNumber">??</b> 개
         </span>
         <span className="MyRight">
           <button className="RightQuiz" type="button">
             맞춘 문제 확인
           </button>
-          <b>??</b> 개
+          <b className="rNumber">??</b> 개
         </span>
         <span className="MyWrong">
           <button className="WrongQuiz" type="button">
             틀린 문제 확인
           </button>
-          <b>??</b> 개
+          <b className="wNumber">??</b> 개
         </span>
       </div>
     </div>
