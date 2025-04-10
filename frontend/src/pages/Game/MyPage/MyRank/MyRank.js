454794e738ba1cf19b644e62b0e6e0dba173f361
Jeon Juhyun <juh060415@gmail.com>
2025-04-10 22:59:25 +0900

    feat. 랭킹부분 수정

--- frontend/src/pages/Game/MyPage/MyRank/MyRank.js
+++ frontend/src/pages/Game/MyPage/MyRank/MyRank.js
@@ -6,6 +6,19 @@ function My_Rank() {
     <div className="MyRank">
       <div className="MyRanking">
         <div className="Ranking">👑MY RANKING👑</div>
+        <div className="Rank">
+          <span className="Placing">
+            <b className="MyPlacing">???</b> 등
+          </span>
+          <span className="Ranker">NICKNAME</span>
+          <span className="Scoreline">
+            <b className="aNumber">???</b>/<b className="rNumber">???</b>/
+            <b className="wNumber">???</b>
+          </span>
+          <span className="Percent">
+            <b className="r">XX.xx</b> %
+          </span>
+        </div>
       </div>
       <div className="UserRanking"></div>
     </div>
