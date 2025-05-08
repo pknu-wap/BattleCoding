b30e8d4d399d180843bf7af8115dd9fed6191928
Jeong Yurim <yr61oo@naver.com>
2025-05-08 19:05:51 +0900

    feat: ip 주소 변경 수정

--- frontend/src/pages/Login/components/LoginInputForm.jsx
+++ frontend/src/pages/Login/components/LoginInputForm.jsx
@@ -53,7 +53,7 @@ const LoginInputForm = () => {
         };
 
         try {
-            const response = await axios.post("http://localhost:8080/auth/login", loginData);
+            const response = await axios.post(`${process.env.REACT_APP_API_URL}/auth/login`, loginData);
             
             if (response.data.success) {
                 const token = response.data.token;
