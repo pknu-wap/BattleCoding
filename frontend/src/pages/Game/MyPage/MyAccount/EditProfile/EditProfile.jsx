ff607484fa5ab1b3630f19cc9d9f8b00095812c0
PKJYJ <p134812024@gmail.com>
2025-05-19 21:30:50 +0900

    fix: edit-frofile 디자인 수정, 프로필 사진 추가

--- frontend/src/pages/Game/MyPage/MyAccount/EditProfile/EditProfile.jsx
+++ frontend/src/pages/Game/MyPage/MyAccount/EditProfile/EditProfile.jsx
@@ -3,15 +3,16 @@ import { useNavigate } from 'react-router-dom';
 import './EditProfile.scss';
 
 const profileImages = [
-  'https://cdn.pixabay.com/photo/2025/04/30/03/22/popsicle-9568309_1280.jpg',
-  'https://cdn.pixabay.com/photo/2025/05/04/17/47/dog-9578735_1280.jpg',
-  'https://cdn.pixabay.com/photo/2025/03/25/15/57/flowers-9492798_1280.jpg',
-  'https://cdn.pixabay.com/photo/2025/03/23/22/04/bluejay-9489628_1280.jpg',
-  'https://cdn.pixabay.com/photo/2024/05/15/20/57/developer-8764521_1280.jpg',
-  'https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720_1280.jpg',
-  'https://cdn.pixabay.com/photo/2024/04/09/03/04/ai-generated-8684869_1280.jpg',
-  'https://cdn.pixabay.com/photo/2024/01/24/21/41/woman-8530607_1280.png',
-  'https://cdn.pixabay.com/photo/2019/09/16/20/21/cat-cat-4481997_1280.jpg'
+  'https://res.cloudinary.com/dmby7fmvo/image/upload/v1747656353/%EA%B8%B0%EB%B3%B8_%ED%94%84%EB%A1%9C%ED%95%84_o4xxyn.png',
+  'https://res.cloudinary.com/dmby7fmvo/image/upload/v1747656555/%EB%B0%B1%EA%B2%BD%EC%9D%B4_%EA%B8%B0%EB%B3%B8%ED%98%95_yduydu.png',
+  'https://res.cloudinary.com/dmby7fmvo/image/upload/v1747656555/%EB%BF%8C%EA%B3%B5%EC%9D%B4_%EA%B8%B0%EB%B3%B8%ED%98%95_mmomzd.png',
+  'https://res.cloudinary.com/dmby7fmvo/image/upload/v1747656355/%EC%BD%94%EB%94%A9%ED%95%98%EB%8A%94%EB%B0%B1%EA%B2%BD%EC%9D%B4_iqgavf.png',
+  'https://res.cloudinary.com/dmby7fmvo/image/upload/v1747656355/%EC%A7%B1%EA%B5%AC%EB%B0%B1%EA%B2%BD_u2evjx.png',
+  'https://res.cloudinary.com/dmby7fmvo/image/upload/v1747656354/%EB%B0%B1%EA%B2%BD%EC%9D%B4_%EC%8B%A4%EC%82%AC_mljnfn.png',
+  'https://res.cloudinary.com/dmby7fmvo/image/upload/v1747656355/%EC%A7%80%EB%B8%8C%EB%A6%AC%EB%B0%B1%EA%B2%BD_mhmfpg.png',
+  'https://res.cloudinary.com/dmby7fmvo/image/upload/v1747656355/%EC%A7%80%EB%B8%8C%EB%A6%AC%EB%82%A8_mv1kqa.png',
+  'https://res.cloudinary.com/dmby7fmvo/image/upload/v1747656355/%EC%A7%80%EB%B8%8C%EB%A6%AC%EB%85%80_jc2cc9.png',
+  'https://res.cloudinary.com/dmby7fmvo/image/upload/v1747656357/%EC%A7%80%EB%B8%8C%EB%A6%AC%EC%97%AC%EC%BA%90_xiff0j.png'
 ];
 
 function EditProfile() {
@@ -35,7 +36,7 @@ function EditProfile() {
       localStorage.setItem('password', password);
     }
 
-    navigate('/mypage/edit');
+    navigate('/mypage');
   };
 
   return (
@@ -86,7 +87,7 @@ function EditProfile() {
 
         <div className="buttons">
           <button type="submit">저장</button>
-          <button type="button" onClick={() => navigate('/mypage')}>
+          <button type="cancel" onClick={() => navigate('/mypage')}>
             취소
           </button>
         </div>
