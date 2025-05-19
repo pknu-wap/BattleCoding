2026f023efe283bc046c537332f62b0a30d5738a
Jeon Juhyun <juh060415@gmail.com>
2025-05-19 21:57:11 +0900

    feat. 랭킹페이지 효과

---
+++ frontend/src/pages/Game/Game_main/Game_Rback.jsx
@@ -0,0 +1,9 @@
+import "./Game_Rback.scss";
+
+export default function RBack() {
+  return (
+    <section className="rankingmodebackground">
+      <div className="r_background"></div>
+    </section>
+  );
+}
